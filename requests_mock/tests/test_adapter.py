6160891b1defd2b1c74c7bb3eb86d494631d0aff
Simon Taranto <simon@globality.com>
2015-05-08 15:01:51 -0700

    Small typo fix - s/adatper/adapter

--- requests_mock/tests/test_adapter.py
+++ requests_mock/tests/test_adapter.py
@@ -353,7 +353,7 @@ class SessionAdapterTests(base.TestCase):
         self.assertEqual(len(resps), self.adapter.call_count)
         self.assertTrue(self.adapter.called)
 
-    def test_adapter_picks_correct_adatper(self):
+    def test_adapter_picks_correct_adapter(self):
         good = '%s://test3.url/' % self.PREFIX
         self.adapter.register_uri('GET',
                                   '%s://test1.url' % self.PREFIX,
